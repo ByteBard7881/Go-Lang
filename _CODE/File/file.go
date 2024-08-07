35a39a92cff61844fddfa1c5bd8fbb97e5b61845
ByteBard7881 <adityaps2004@gmail.com>
2024-08-08 03:19:22 +0530

    Go Language

--- _CODE/File/file.go
+++ _CODE/File/file.go
@@ -26,26 +26,26 @@ func main() {
 	fmt.Println("[+] File Created.")
 
 	// Reading file using buffer
-	file, err3 := os.Open("example.txt")
+	// file, err3 := os.Open("example.txt")
 
-	if err3 != nil {
-		fmt.Println("[-] Error while creating file: ", err3)
-		return
-	}
-	defer file.Close()
+	// if err3 != nil {
+	// 	fmt.Println("[-] Error while creating file: ", err3)
+	// 	return
+	// }
+	// defer file.Close()
 
-	buffer := make([]byte, 1024)
-	for {
-		n, err4 := file.Read(buffer)
-		if err4 == io.EOF {
-			break
-		}
-		if err4 != nil {
-			fmt.Println("[-] Error while reading file: ", err4)
-			return
-		}
-		fmt.Println("Reading file using buffer: " + string(buffer[:n]))
-	}
+	// buffer := make([]byte, 1024)
+	// for {
+	// 	n, err4 := file.Read(buffer)
+	// 	if err4 == io.EOF {
+	// 		break
+	// 	}
+	// 	if err4 != nil {
+	// 		fmt.Println("[-] Error while reading file: ", err4)
+	// 		return
+	// 	}
+	// 	fmt.Println("Reading file using buffer: " + string(buffer[:n]))
+	// }
 
 	// Reading file using ioutil
 	// content, err5 := os.ReadFile("example.txt")
