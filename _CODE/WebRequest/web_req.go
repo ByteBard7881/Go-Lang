b2eb52ece0fcf4ddeadaabe8ef5cca8086f1f485
ByteBard7881 <adityaps2004@gmail.com>
2024-08-08 03:35:00 +0530

    Go Language

--- _CODE/WebRequest/web_req.go
+++ _CODE/WebRequest/web_req.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func main() {
+	get_response, get_err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+
+	if get_err != nil {
+		fmt.Println("[-] Error recieved in JSON Get Request: ", get_err)
+		return
+	}
+	defer get_response.Body.Close()
+
+	// Read Response
+	data, data_err := ioutil.ReadAll(get_response.Body)
+	if data_err != nil {
+		fmt.Println("[-] Error recieved in JSON Get Request: ", data_err)
+		return
+	}
+	fmt.Println("HTTP Get JSON Response:\n" + string(data))
 }
