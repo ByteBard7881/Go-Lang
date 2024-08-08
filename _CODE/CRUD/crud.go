4071cc405eeb4421b560675d1643c47000dfb32c
ByteBard7881 <adityaps2004@gmail.com>
2024-08-08 13:52:10 +0530

    Go Language

--- _CODE/CRUD/crud.go
+++ _CODE/CRUD/crud.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type FakeAPI struct {
@@ -16,9 +17,10 @@ type FakeAPI struct {
 
 func main() {
 	fmt.Println("CRUD Operations")
-	url := "https://jsonplaceholder.typicode.com/todos/1"
-	get(url)
-	post(url)
+	post_url := "https://jsonplaceholder.typicode.com/todos"
+	get_url := "https://jsonplaceholder.typicode.com/todos/1"
+	get(get_url)
+	post(post_url)
 }
 
 func get(get_url string) {
@@ -38,12 +40,7 @@ func get(get_url string) {
 		return
 	}
 
-	get_data, get_data_error := ioutil.ReadAll(get_response.Body)
-
-	if get_data_error != nil {
-		fmt.Println("[-] Error recieved in JSON Get Request: ", get_data_error)
-		return
-	}
+	get_data, _ := ioutil.ReadAll(get_response.Body)
 
 	json_decoded_error := json.Unmarshal(get_data, &get_fake_api)
 
@@ -64,8 +61,6 @@ func post(post_url string) {
 		Completed: true,
 	}
 
-	fmt.Println(post_url)
-
 	post_fake_api, post_data_error := json.Marshal(post_fake_data)
 
 	if post_data_error != nil {
@@ -73,5 +68,17 @@ func post(post_url string) {
 		return
 	}
 
-	fmt.Println(string(post_fake_api))
+	post_fake_api_reader := strings.NewReader(string(post_fake_api))
+
+	post_response, post_error := http.Post(post_url, "application/json", post_fake_api_reader)
+
+	if post_error != nil {
+		fmt.Println("[-] Error while performing post method: ", post_error)
+		return
+	}
+
+	defer post_response.Body.Close()
+
+	post_readable_format, _ := ioutil.ReadAll(post_response.Body)
+	fmt.Println(string(post_readable_format))
 }
